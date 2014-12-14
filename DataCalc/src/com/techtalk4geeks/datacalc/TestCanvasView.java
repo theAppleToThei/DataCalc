a995b2e7dc43d517c4393ac13c1aa546bb960175
Alex Baratti <alex.techtalk4geeks@gmail.com>
2014-12-13 22:03:28 -0800

    Arcs now work

--- DataCalc/src/com/techtalk4geeks/datacalc/TestCanvasView.java
+++ DataCalc/src/com/techtalk4geeks/datacalc/TestCanvasView.java
@@ -11,7 +11,6 @@ import android.view.View;
 
 public class TestCanvasView extends View
 {
-
 	float drawOver = 39;
 
 	public TestCanvasView(Context context, AttributeSet set)
@@ -30,17 +29,34 @@ public class TestCanvasView extends View
 
 		int centerX = (int) (360);
 		int centerY = (int) (640);
-		RectF oval = new RectF(centerX, centerY, centerX, centerY);
-		paint.setColor(Color.RED); // Transparent
+		RectF oval = new RectF(centerX, centerY, centerX + 200, centerY + 200);
+		paint.setColor(Color.GREEN);
+		canvas.drawArc(oval, 0, 45, true, paint);
+		paint.setColor(Color.RED);
 		canvas.drawArc(oval, 45, drawOver, true, paint);
+		paint.setColor(Color.YELLOW);
+		canvas.drawArc(oval, 84, 276, true, paint);
 
 		paint.setTextSize(45);
 		paint.setColor(Color.RED);
 		canvas.drawText("Something is working!", 50, 85, paint);
 		paint.setColor(Color.YELLOW);
-		canvas.drawText("Something is working!", 50, 75, paint);
+		canvas.drawText("Something is working!", 50, 135, paint);
 		paint.setColor(Color.GREEN);
-		canvas.drawText("Something is working!", 50, 65, paint);
+		canvas.drawText("Something is working!", 50, 175, paint);
+		paint.setColor(Color.BLUE);
+		canvas.drawText("Something is working!", 50, 215, paint);
+		paint.setColor(Color.MAGENTA);
+		canvas.drawText("Something is working!", 50, 255, paint);
+		paint.setColor(Color.GRAY);
+		canvas.drawText("Something is working!", 50, 295, paint);
+		paint.setColor(Color.BLACK);
+		canvas.drawText("Something is working!", 50, 335, paint);
+		
+		canvas.drawCircle(50, 500, 5, paint);
+		canvas.drawCircle(65, 500, 10, paint);
+		canvas.drawCircle(90, 500, 15, paint);
+		canvas.drawCircle(120, 500, 20, paint);
 		canvas.save();
 	}
 
